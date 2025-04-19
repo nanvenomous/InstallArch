3181436bf8058b59f73b73283116ee926b0ded76
nanvenomous <mrgarelli@gmail.com>
2025-04-19 16:57:19 -0500

    refactor: home is no longer mounted under root it's one partition add: many more automations post install

--- utils/partitioning.sh
+++ utils/partitioning.sh
@@ -2,10 +2,7 @@
 
 disk="${1}"
 bootSize="1" # usually 1
-rootSize="200" # usually 20
 swapSize="8" # usually rest of disk
-
-homeSize="" # default is to end of disk
 
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${disk}
 	g # clear the in memory partition table
@@ -15,24 +12,20 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${disk}
 	+${bootSize}G # boot partition size
 	t # type of the partition
 	1 # 1 is the type for EFI
-	n # new root partition
-	2 # partition number 2
-	  # default, start immediately after preceding partition
-	+${rootSize}G # root size
-	t # type of the partition
-	2 # partition number 2
-	20 # linux filesystem
 	n # new swap partition
-	3 # partition number 3
+	2 # partition number 2
 	  # default, start immediately after preceding partition
 	+${swapSize}G # swap size
 	t # type of the partition
-	3 # partition number 3
+	2 # partition number 2
 	19 # linux swap
-	n # new partition
-	4 # partion number 4
+	n # new root partition
+	3 # partition number 3
 	  # default, start immediately after preceding partition
-	${homeSize} # default, extend partition to end of disk
+	  # default, extend partition to end of disk
+	t # type of the partition
+	3 # partition number 3
+	20 # linux filesystem
 	p # print the in-memory partition table
 	w # write the partition table
 	q # and we're done
